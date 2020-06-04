0eb547fb100d4600c6bb254f72913adbf28ca0f2
Andrew Steele <andrew565@gmail.com>
2020-06-03 21:24:25 -0400

    --- Auto-Git Commit ---

--- parseStatistics.ts
+++ parseStatistics.ts
@@ -1,5 +1,6 @@
 import { StatisticsObject } from "./StatisticsObject";
 
 export const parseStats = (HallOfStatistics: StatisticsObject[]) => {
-  
+  const numberOfStats = HallOfStatistics.length;
+  const averageNumberOfTurns = HallOfStatistics.reduce
 };
